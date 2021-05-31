d2dfd8e3432887ea58e77fb0c8442f86232e0d94
Rushabh Jain <Rushabh.Jain@vcontractor.co.za>
2021-05-31 10:20:30 +0530

    Changed baseurl to live

--- config/index.js
+++ config/index.js
@@ -1,7 +1,5 @@
-// const baseurl = 'http://localhost:8080';
-export const baseurl = 'http://10.0.2.2:8080';
-// const baseurl = 'https://sportsgeek.herokuapp.com';
-// export const baseurl = 'https://sports-geek.herokuapp.com';
+// export const baseurl = 'http://10.0.2.2:8080';
+export const baseurl = 'https://sports-geek.herokuapp.com';
 
 // export default baseurl;
 
